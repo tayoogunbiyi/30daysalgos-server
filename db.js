c6b3cda3d1b741a19748ba76740c0d1eb6e563ba
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-04-10 17:11:08 +0100

    removed hard coded url

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ const { DB_URL } = require("./constants/db");
 const mongoose = require("mongoose");
 
 mongoose.set("useCreateIndex", true);
-const localUrl = "mongodb://localhost:27017/30days"
 
-mongoose.connect(DB_URL || localUrl, { useNewUrlParser: true });
+mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 module.exports = { db: mongoose.connection };
