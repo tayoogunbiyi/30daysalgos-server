b980aa4bd0ea3e323d85a6c2063ee1211f9ba44e
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-04-13 10:10:19 +0100

    added endpont to fetch all questions

--- routes/questionRoutes/index.js
+++ routes/questionRoutes/index.js
@@ -10,8 +10,6 @@ const Question = mongoose.model('Question');
 const router = express.Router();
 
 
-
-
 router.get('/',async (req,res) => {
     try {
     const questionsBeforeCurrTime = await Question.getAllQuestionsBefore(Date.now());
@@ -27,6 +25,23 @@ router.get('/',async (req,res) => {
             buildResponse(error.message || messages.SERVER_ERROR,null,false)
         )
     }
+})
+
+router.get('/all',async (req,res) => {
+    try {
+        const allQuestions = await Question.find({})
+        return res.json(
+            buildResponse(
+                `Fetched all questions ${messages.SUCCESS_MESSAGE}`,
+                allQuestions,
+                true
+            )
+        )
+        } catch (error) {
+            return res.status(500).json(
+                buildResponse(error.message || messages.SERVER_ERROR,null,false)
+            )
+        }
 })
 
 router.post('/',isAdminOrGreater, async (req,res) => {
