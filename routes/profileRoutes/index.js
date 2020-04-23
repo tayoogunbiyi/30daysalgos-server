e00d8b975c52778e0ec8976711e4a3dc75fc17e5
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-04-23 12:40:29 +0100

    implemented profile update

--- routes/profileRoutes/index.js
+++ routes/profileRoutes/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
-
-// const User = mongoose.model("User");
+const { joiValidate } = require("express-joi");
+const { userUpdateSchema } = require("../../validation/validationSchemas");
+const messages = require("../../services/responseMessages");
+const User = mongoose.model("User");
 
 const { buildResponse } = require("../../services/responseBuilder");
 
@@ -15,6 +17,20 @@ router.get("/", async (req, res) => {
   return res.json(
     buildResponse(`Profile fetched succesfully.`, req.user, true)
   );
+});
+
+router.put("/", joiValidate(userUpdateSchema), async (req, res) => {
+  if (!req.user) {
+    return res.json(buildResponse(`Unable to fetch profile`, null, false));
+  }
+  try {
+    const user = await User.updateUser(req.user.id, req.body);
+    return res.json(buildResponse(`Updated profile`, user, true));
+  } catch (error) {
+    res
+      .status(400)
+      .json(buildResponse(error.message || messages.SERVER_ERROR), null, false);
+  }
 });
 
 module.exports = router;
