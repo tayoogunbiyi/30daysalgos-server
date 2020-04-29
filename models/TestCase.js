8742005b248a78ac17dfe130b077ef7aa4c421d3
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-04-29 19:12:16 +0100

    implemented testAgainst function

--- models/TestCase.js
+++ models/TestCase.js
@@ -17,6 +17,24 @@ const TestCaseSchema = new Schema({
   },
 });
 
+TestCaseSchema.statics.testAgainst = async function (questionId, userSolution) {
+  const testCases = await this.find({ question: questionId });
+  if (testCases.length != userSolution.length) {
+    throw new Error(
+      `Expected input of length ${testCases.length} and got length of ${userSolution.length}`
+    );
+  }
+  let passedTestCases = 0;
+  for (let j = 0; j < testCases.length; j++) {
+    const output = userSolution[j];
+    const { expectedOutput } = testCases[j];
+    if (output === expectedOutput) {
+      passedTestCases += 1;
+    }
+  }
+  return { passedTestCases, totalTestCases: testCases.length };
+};
+
 TestCaseSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj["__v "];
