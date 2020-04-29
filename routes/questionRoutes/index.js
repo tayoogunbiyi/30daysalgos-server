f6ea0fb1cf3ebd45fd3367b9bf47b9117a06427d
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-04-29 19:54:51 +0100

    impleted CRD ops for testcases

--- routes/questionRoutes/index.js
+++ routes/questionRoutes/index.js
@@ -15,6 +15,7 @@ const {
   questionUpdateSchema,
   exampleSchema,
   exampleUpdateSchema,
+  testCaseSchema,
 } = require("../../validation/validationSchemas");
 const messages = require("../../services/responseMessages");
 const { buildResponse } = require("../../services/responseBuilder");
@@ -22,6 +23,7 @@ const { buildResponse } = require("../../services/responseBuilder");
 const { FILE_UPLOADER_CONFIG } = require("../../constants/uploads");
 
 const { buildDuplicateMessage } = messages;
+const TestCase = mongoose.model("TestCase");
 const Question = mongoose.model("Question");
 const Example = mongoose.model("Example");
 const Submission = mongoose.model("Submission");
@@ -277,6 +279,82 @@ router.delete("/examples/:id", isAdminOrGreater, async (req, res) => {
       buildResponse(
         `Deleted question with ${id} ${messages.SUCCESS_MESSAGE}`,
         null,
+        true
+      )
+    );
+  } catch (error) {
+    res
+      .status(400)
+      .json(buildResponse(error.message || messages.SERVER_ERROR), null, false);
+  }
+});
+
+router.post(
+  "/:id/testcase",
+  isAdminOrGreater,
+  joiValidate(testCaseSchema),
+  async (req, res) => {
+    const { id } = req.params;
+    try {
+      await checkValidId(id, Question);
+      const testcase = await TestCase.create({ ...req.body, question: id });
+      return res
+        .status(201)
+        .json(
+          buildResponse(
+            `Created testcase ${messages.SUCCESS_MESSAGE}`,
+            testcase,
+            true
+          )
+        );
+    } catch (error) {
+      return res
+        .status(400)
+        .json(
+          buildResponse(error.message || messages.SERVER_ERROR, null, false)
+        );
+    }
+  }
+);
+
+router.delete(
+  "/:id/testcase/:testCaseId",
+  isAdminOrGreater,
+  async (req, res) => {
+    try {
+      const { id, testCaseId } = req.params;
+      await checkValidIdOnObj(id, Question);
+      await checkValidIdOnObj(testCaseId, TestCase);
+      const result = await TestCase.deleteTestCase(testCaseId);
+      if (!result.deletedCount) throw Error("Unable to delete testcase");
+      return res.json(
+        buildResponse(
+          `Deleted testcase with ${testCaseId} ${messages.SUCCESS_MESSAGE}`,
+          null,
+          true
+        )
+      );
+    } catch (error) {
+      res
+        .status(400)
+        .json(
+          buildResponse(error.message || messages.SERVER_ERROR),
+          null,
+          false
+        );
+    }
+  }
+);
+
+router.get("/:id/testcase", isAdminOrGreater, async (req, res) => {
+  try {
+    const { id } = req.params;
+    await checkValidIdOnObj(id, Question);
+    const testCases = await TestCase.find({ question: id });
+    return res.json(
+      buildResponse(
+        `Fetched testcases ${messages.SUCCESS_MESSAGE}`,
+        testCases,
         true
       )
     );
